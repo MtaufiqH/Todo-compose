46f773256f07b4ede638e5e377d6f534ccd0bece
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2021-09-16 11:07:44 +0800

    create todo Repository

---
+++ app/src/main/java/taufiq/apps/todo_compose/data/repositories/TodoRepository.kt
@@ -0,0 +1,41 @@
+package taufiq.apps.todo_compose.data.repositories
+
+import kotlinx.coroutines.flow.Flow
+import taufiq.apps.todo_compose.data.TodoDao
+import taufiq.apps.todo_compose.data.model.TodoTask
+import javax.inject.Inject
+
+/**
+ * Created By Taufiq on 9/16/2021.
+ *
+ */
+class TodoRepository @Inject constructor(private val todoDao: TodoDao) {
+
+    val allTasks = todoDao.getAllTask()
+    val sortByLowPriority = todoDao.sortByLowPriority()
+    val sortByHighPriority = todoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<TodoTask> =
+        todoDao.getSelectedTask(taskId = taskId)
+
+    suspend fun addTask(todoTask: TodoTask) {
+        todoDao.addTask(todoTask)
+    }
+
+    suspend fun updateTask(todoTask: TodoTask) {
+        todoDao.updateTask(todoTask)
+    }
+
+    suspend fun deleteTask(todoTask: TodoTask) {
+        todoDao.deleteTask(task = todoTask)
+    }
+
+    suspend fun deleteAllTask(todoTask: TodoTask) {
+        todoDao.deleteAllTask()
+    }
+
+    suspend fun searchTask(query: String) {
+        todoDao.searchTasks(query)
+    }
+
+}
